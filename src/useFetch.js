cd021264cff89bc6601d3fbef01c1ef1f02172b1
Afaggian <95209107+Afaggian@users.noreply.github.com>
2022-06-08 20:59:20 +1000

    Creating Routers for the Website

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
   
     useEffect(() => {
-      fetch(url)
+      const abortCont = new AbortController();
+
+      fetch(url, { signal: abortCont.signal })
         .then(res => {
           if (!res.ok) { // error coming back from server
             throw Error('Could not fetch the data for that resource');
@@ -18,11 +20,17 @@ const useFetch = (url) => {
           setIsPending(false);
         })
         .catch(err =>{
+          if ( err.name === "AbortError") {
+            console.log("fetch aborted")
+          }
+          else {
              // auto catches network / connection error
           setIsPending(false);  
           setError(err.message);
-          
+          }
         })
+
+      return () => abortCont.abort();
     }, [url]);
     return {data, isPending, error};
 }
